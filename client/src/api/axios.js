923ba7e5a0cde38124a597cc71bc5327bf7cb9a9
Aashish-Parmar <aashishparmarthree45@gmail.com>
2025-08-02 23:24:56 +0530

    Initial commit database1

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,10 +2,16 @@
 // src/api/axios.js
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
+//   withCredentials: false // enable if you use cookies for auth
+// });
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://ahmedabad-career-hub.onrender.com/api",
   withCredentials: false // enable if you use cookies for auth
 });
+
 
 // Optionally: attach token to all requests automatically
 api.interceptors.request.use(
